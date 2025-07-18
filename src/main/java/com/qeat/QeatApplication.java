7f76c92ba586eea66a4d48ab0b59dfbc6f517911
sunhanmoo <ghdms1013@naver.com>
2025-07-18 16:51:35 +0900

    🔧 Setting: 프로젝트 구조 설정

--- src/main/java/com/qeat/QeatApplication.java
+++ src/main/java/com/qeat/QeatApplication.java
@@ -3,9 +3,11 @@ package com.qeat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 @SpringBootApplication(
-		scanBasePackages = {"com.qeat.admin", "com.qeat.core", "com.qeat.global"}
+		scanBasePackages = {"com.qeat.global"}
 )
 @ConfigurationPropertiesScan(basePackages = "com.qeat")
 public class QeatApplication {
