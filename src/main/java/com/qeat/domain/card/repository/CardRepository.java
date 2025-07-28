72053bc4c57590eb2ec715da29d19871964d54a4
sunhanmoo <ghdms1013@naver.com>
2025-07-28 12:26:36 +0900

    ✨ Feature: 카드 등록 API

---
+++ src/main/java/com/qeat/domain/card/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package com.qeat.domain.card.repository;
+
+import com.qeat.domain.card.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
