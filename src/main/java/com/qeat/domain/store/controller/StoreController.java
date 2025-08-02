a57daa16f37e9c73fbfffa91a0abea4c6b555d2a
sunhanmoo <ghdms1013@naver.com>
2025-08-02 20:24:43 +0900

    🔨 Refactor: 가게 API 테스트

--- src/main/java/com/qeat/domain/store/controller/StoreController.java
+++ src/main/java/com/qeat/domain/store/controller/StoreController.java
@@ -5,10 +5,12 @@ import com.qeat.domain.store.dto.response.StoreDetailResponse;
 import com.qeat.domain.store.service.StoreBookmarkService;
 import com.qeat.domain.store.service.StoreService;
 import com.qeat.global.apiPayload.CustomResponse;
+import com.qeat.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,23 +32,32 @@ public class StoreController {
     }
 
     @PostMapping("/{storeId}/bookmark")
-    @Operation(summary = "가게 북마크 API", description = "가게를 북마크합니다.")
+    @Operation(summary = "가게 저장 API", description = "가게를 저장합니다.")
     public ResponseEntity<CustomResponse<Void>> bookmarkStore(@PathVariable Long storeId) {
-        storeBookmarkService.bookmarkStore(storeId);
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getUserId();
+
+        storeBookmarkService.bookmarkStore(userId, storeId);
         return ResponseEntity.ok(CustomResponse.<Void>onSuccess(null, "OK"));
     }
 
     @DeleteMapping("/{storeId}/bookmark")
     @Operation(summary = "가게 저장 해제 API", description = "사용자가 특정 가게의 저장을 해제합니다.")
     public ResponseEntity<CustomResponse<Void>> unbookmarkStore(@PathVariable Long storeId) {
-        storeBookmarkService.unbookmarkStore(storeId);
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getUserId();
+
+        storeBookmarkService.unbookmarkStore(userId, storeId);
         return ResponseEntity.ok(CustomResponse.<Void>onSuccess(null, "OK"));
     }
 
     @GetMapping("/bookmark")
     @Operation(summary = "저장한 가게 리스트 조회 API", description = "북마크한 가게 목록을 반환합니다.")
     public ResponseEntity<CustomResponse<List<StoreBookmarkListResponse>>> getBookmarkedStores() {
-        List<StoreBookmarkListResponse> result = storeBookmarkService.getBookmarkedStores();
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = userDetails.getUserId();
+
+        List<StoreBookmarkListResponse> result = storeBookmarkService.getBookmarkedStores(userId);
         return ResponseEntity.ok(CustomResponse.onSuccess(result, "OK"));
     }
 }
--- src/main/java/com/qeat/global/apiPayload/exception/CustomException.java
+++ src/main/java/com/qeat/global/apiPayload/exception/CustomException.java
@@ -9,6 +9,7 @@ public class CustomException extends RuntimeException{
     private final BaseErrorCode code;
 
     public CustomException(BaseErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.code = errorCode;
     }
 
